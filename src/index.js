b21e397d77234a74dad7d7b235a36141836346dc
cl98561 <ludwigconnorj@johndeere.com>
2022-12-24 08:07:38 -0600

    Fix header line issue and use google analytics.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './static/styles/pages/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import TagManager from 'react-gtm-module'
+const tagManagerArgs = {
+  gtmId: 'G-DF08RM7MCG'
+}
+TagManager.initialize(tagManagerArgs);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -14,4 +20,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// reportWebVitals();
+// reportWebVitals(console.log);
