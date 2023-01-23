16c2f435d9af874bd1d152e4d1c285035f5f7715
Connor Ludwig <49032961+cjlludwig@users.noreply.github.com>
2023-01-22 18:54:57 -0600

    Adding basic Github actions Cicd (#6)
    * Adding actions.
    
    * Fix naming
    
    * Adding Basic CI testing and basic snapshot tests.
    
    * Split steps
    
    Co-authored-by: cl98561 <ludwigconnorj@johndeere.com>

---
+++ tests/App.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import renderer from "react-test-renderer";
+import App from '../src/App';
+
+import ContactCard from "../src/components/ContactCard"
+import LinkCatalog from "../src/components/LinkCatalog"
+import Resume from "../src/components/Resume"
+import Welcome from "../src/components/Welcome"
+import Header from "../src/components/Header"
+
+jest.mock("../src/components/ContactCard");
+jest.mock("../src/components/LinkCatalog");
+jest.mock("../src/components/Resume");
+jest.mock("../src/components/Welcome");
+jest.mock("../src/components/Header");
+
+describe("<App />", () => {
+  test("renders correctly", () => {
+    const tree = renderer
+      .create(<App />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
