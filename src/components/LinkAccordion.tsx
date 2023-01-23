89302e59fa4f8158c076e41d8ae31f2db62af8ac
cl98561 <ludwigconnorj@johndeere.com>
2023-01-22 18:48:46 -0600

    Adding Basic CI testing and basic snapshot tests.

--- src/components/LinkAccordion.tsx
+++ src/components/LinkAccordion.tsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 import Accordion from 'react-bootstrap/Accordion';
 import { Links } from '../models/links';
-import linkPic from '../static/images/web-link.png';
 import { Card } from "react-bootstrap";
+
+import linkPic from '../static/images/web-link.png';
+
 import '../static/styles/components/LinkAccordion.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function LinkAccordion<Props extends { links: Links[] }>(props: Props) {
+export interface Props {
+  links: Links[];
+}
+
+function LinkAccordion(props: Props) {
   const { links=[] } = props;
   const [ openKey, setOpen ] = useState<number | null>(null);
 
   return (
-    <Accordion >
+    <Accordion data-testid="link-accordion" >
     {
       links.map((data, index) => {
         const { name, url, description } = data;
