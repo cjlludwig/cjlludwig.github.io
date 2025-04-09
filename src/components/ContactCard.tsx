0425e9b58d4454938476ba7c0749ad37829d417a
Connor Ludwig <cjlludwig@gmail.com>
2025-04-09 10:48:07 -0500

    Card

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -5,27 +5,26 @@ function Card() {
   return (
     <div className='contact-card-wrapper'>
       <div className="contact-card">
-        <div className="top">
-          <h2 className="name">Connor J Ludwig</h2>
-          <img className="circle-img" src={headshot} alt="avatar_img" />
+        <div className="contact-header">
+          <img className="profile-img" src={headshot} alt="Connor Ludwig" />
+          <div className="header-content">
+            <h2 className="name">Connor J Ludwig</h2>
+            <p className="title">Staff Software Engineer</p>
+          </div>
         </div>
-        <div className="bottom">
-          <p className="info">
-            <b>Cell: </b>
-            &emsp;&emsp;(712)-830-6202
-          </p>
-            <b>Email: </b>
-            &emsp;&ensp;<a href = "mailto: cjlludwig@gmail.com">cjlludwig@gmail.com</a>
-          <p className="info">
-          </p>
-          <p className="info">
-            <b>Github: </b>
-            &emsp;<a href="https://github.com/cjlludwig">https://github.com/cjlludwig</a>
-          </p>
-          <p className="info">
-            <b>Linkedin: </b>
-            &nbsp;<a href="www.linkedin.com/in/connor-ludwig/">www.linkedin.com/in/connor-ludwig/</a>
-          </p>
+        <div className="contact-content">
+          <div className="contact-item">
+            <i className="fas fa-envelope"></i>
+            <a href="mailto:cjlludwig@gmail.com">cjlludwig@gmail.com</a>
+          </div>
+          <div className="contact-item">
+            <i className="fab fa-github"></i>
+            <a href="https://github.com/cjlludwig" target="_blank" rel="noopener noreferrer">github.com/cjlludwig</a>
+          </div>
+          <div className="contact-item">
+            <i className="fab fa-linkedin"></i>
+            <a href="https://www.linkedin.com/in/connor-ludwig/" target="_blank" rel="noopener noreferrer">linkedin.com/in/connor-ludwig</a>
+          </div>
         </div>
       </div>
     </div>
